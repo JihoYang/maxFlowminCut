ea389e789986d4c469f874a127abf152a0ea5b5b
Jorge <jasm915@gmail.com>
2017-09-22 13:57:04 +0200

    Added .bk reader

---
+++ read_file/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "read_bk.h"
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    if (argc <= 1)
+	{
+		printf("Usage: %s <filename>\n", argv[0]);
+		return 1;
+    }
+    
+    read_bk<float> *g = new read_bk<float>(argv[1]); 
+    int numNodes  = g->nNodes;
+    float *f = g->f;
+    float **w = g->w;
+    /*
+    cout << "\nIn main\n" <<endl;
+    cout << "Printing f" <<endl;
+    for (int n = 0; n<numNodes; n++)
+        cout<<"f_"<<n<<" = " << f[n]<< endl;
+    
+    cout<< "Printing w" <<endl;
+    for (int i = 0; i<numNodes; i++)
+        for (int j = 0; j<numNodes; j++)
+            cout<<"w_{"<<i<<","<<j<<"} = " << w[i][j] << endl;
+    */
+    delete g;
+    
+    return 0;
+}
---
+++ read_file/read_bk.h
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+#ifndef _READ_BK_H
+#define _READ_BK_H
+
+template <class T>
+class read_bk
+{
+    public:    
+        // Variables
+        int nNodes = 0;
+        int nEdges = 0;
+        // Vector of weights per node
+        T *f;
+        // Array of weights, size = nNodes * nNodes
+        T **w;
+
+        // Methods
+        void init_graph(int numberNodes, int numberEdges);
+        void free_memory();
+        bool readFile(char* fileName);
+
+        read_bk(char* filename);
+        
+        ~read_bk();
+        
+};
+#endif
