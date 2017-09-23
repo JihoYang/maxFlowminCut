82d9c4d396a575bb86908017c3d5aa6950307074
Jiho Yang <jy1737@my.bristol.ac.uk>
2017-09-23 16:48:40 +0200

    merged primal dual and read bk to merge-test branch

--- main.cpp
+++ main.cpp
@@ -1,121 +1,42 @@
 #include <iostream>
-#include <math.h>
+#include "read_bk.h"
 using namespace std;
 
-void compute_f(float *f, float *u, float **w, int &num_vertex){
-	for (size_t j = 1; j < num_vertex-1; ++j){
-		float sum1 = 0;
-		float sum2 = 0;
-		f[j] = -w[0][j] + w[j][num_vertex-1];
-		for (size_t i = 1; i < j; ++i){
-			if (i != j){
-				sum1 += w[i][j];
-				sum2 += w[j][i];
-			}
-		}
-		f[j] = f[j] - sum1 + sum2;
-	}
-}
+int main(int argc, char **argv)
+{
+    if (argc <= 1)
+	{
+		printf("Usage: %s <filename>\n", argv[0]);
+		return 1;
+    }
+    
+    read_bk<float> *g = new read_bk<float>(argv[1]); 
+    int numNodes  = g->nNodes;
+    int numEdges = g->nEdges;
+    float *f = g->f;
+    float **w = g->w;
+    int **ord = g->ord;
+    cout << "Number of edges is " << numEdges <<endl;
 
-void compute_w(float **w, int &num_vertex){
-	for (size_t i = 0; i < num_vertex; ++i){
-		for (size_t j = 0; j < num_vertex; ++j){
-			w[i][j] = (w[i][j] + w[j][i])/2;
-			w[j][i] = 0;
-		}
-	}
-}
-
-//Primal-dual algorithm
-void updateX(float *u, float *tau, float *grady, float *y, float *f, float *f_y, int num_vertex){
-	float u_new;
-	for (size_t i = 0; i < num_vertex; i++){
-		u_new = u[i] + tau[i] * (grady[i] - f[i]);
-		f_y[i] = 2*u_new - u[i];	
-		if (u_new < 0)
-			u_new = 0;
-		else if (u_new > 1)
-			u_new = 1;
-	    else if (u_new > 0 && u_new < 0.5)
-			u_new = 0;
-		else if (u_new >= 0.5 && u_new < 1)
-			u_new = 1;
-		u[i] = u_new;
-	}
-}
-
-void updateY(float *y, float *sigma, float *grad_f_y, int num_edge){
-	float y_new;
-	for (size_t i = 0; i < num_edge; i++){
-		y_new = y[i] + sigma[i] * grad_f_y[i];
-		if (y_new < - 1) 
-			y_new = -1;
-		else if (y_new > 1)
-			y_new = 1;
-		else if (y_new > -1 && y_new < 0.5)
-			y_new = -1;
-		else if (y_new < 1 && y_new >= 0.5)
-			y_new = 1;
-		y[i] = y_new;
-	}
-}
-
-void compute_dt(float *tau, float *sigma, float **w, float alpha, float phi, int num_vertex, int num_edge){
-	float lower = min(num_vertex, num_edge);
-	float upper = max(num_vertex, num_edge);
-	for (size_t i = 0; i < lower; i++){
-		// Tau
-		float sum = 0;
-		for (size_t j = 0; j < num_vertex; j++){
-			sum += pow(abs(w[i][j]), alpha);
-		}
-		tau[i] = 1 / (phi * sum);
-		// Sigma
-
-	}
-}
-
-
-int main(int argc, char **argv){
-
-	int num_vertex = 5;
-	int num_edge   = 5;
-	// Memory allocation
-	float *u = new float[num_vertex];
-	float *f = new float[num_vertex];
-	f[0] = 0; f[num_vertex-1] = 0;
-	float **w = new float*[num_vertex];
-	for (size_t i = 0; i < num_vertex; ++i){
-		 w[i] = new float[num_vertex];
-		for (size_t j = 0; j < num_vertex; ++j){
-			w[i][j] = 0;
-		}
-	}
-	// Graph
-	u[0] = 40;
-	u[1] = 0;
-	u[2] = 0;
-	u[3] = -100;
-	u[4] = -40;
-	w[0][1] = 20;
-	w[0][2] = 20;
-	w[2][1] = 30;
-	w[1][3] = 40;
-	w[2][3] = 10;
-	// Compute f
-	compute_f(f, u, w, num_vertex);
-	// Compute omega
-	compute_w(w, num_vertex);
-
-	for (int i=0; i < 4; i++){
-		for (int j=0; j < 4; j++){
-			cout << "w[" << i << "]" << "[" << j << "]" << " = " << w[i][j] << endl;
-		}
-	}
-
-	return 0;
-
-	delete []u;
-	delete []f;
-	delete []w;
+    // For testing only:
+    /*cout << "\nIn main\n" <<endl;
+    for(int i = 0; i<numEdges; i++)
+    {
+        cout<<"ord is "<< ord[i][0] <<", "<< ord[i][1] << endl;
+    } 
+    */
+    /*
+    
+    cout << "Printing f" <<endl;
+    for (int n = 0; n<numNodes; n++)
+        cout<<"f_"<<n<<" = " << f[n]<< endl;
+    
+    cout<< "Printing w" <<endl;
+    for (int i = 0; i<numNodes; i++)
+        for (int j = 0; j<numNodes; j++)
+            cout<<"w_{"<<i<<","<<j<<"} = " << w[i][j] << endl;
+    */
+    delete g;
+    
+    return 0;
 }
---
+++ read_bk.cpp
@@ -0,0 +1,149 @@
+#include "read_bk.h"
+using namespace std;
+
+template<class T>
+read_bk<T>::read_bk(char *filename)
+{
+	readFile(filename);
+} 
+
+template<class T>
+read_bk<T>::~read_bk()
+{
+	free_memory();
+}
+
+template<class T>
+void read_bk<T>::init_graph(int numberNodes, int numberEdges)
+{
+    // Set memory for f(initial node setup) 
+	// and w(weights on the edges)
+	nNodes = numberNodes;
+	nEdges = numberEdges;
+    f = new T[nNodes];
+	w = new T*[nNodes];
+	ord = new int*[nEdges]; 
+	// Set values to 0
+	for(int i = 0; i<nEdges; i++)
+	{
+		ord[i] = new int[2];
+	}	
+	for(int i = 0; i<nNodes; i++)
+	{	
+		f[i] = 0;
+		w[i] = new T[nNodes]; 			  
+		for(int j = 0; j<nNodes; j++)
+		{
+			w[i][j] = 0;
+		}	
+	}	
+}
+
+template<class T>
+void read_bk<T>::free_memory()
+{
+	delete[] f;
+	for(int i = 0; i<nNodes; i++)
+		delete[] w[i];
+	delete[] w; 
+}
+
+
+template<class T>
+bool read_bk<T>::readFile(char *filename)
+{
+	const int MAX_LINE_LEN = 100;
+	char line[MAX_LINE_LEN];
+	int min= 0, max= 0;
+	int declaredNumOfNodes, declaredNumOfEdges, nodeId1, nodeId2;
+	int currentNumOfEdges = 0;
+	char c;
+	// Maybe change this to only integers, depending on the problem..
+	double capacity, capacity2, a, b;
+	int numLines=0;
+	FILE *pFile;
+	
+	if ((pFile = fopen(filename, "r")) == NULL) 
+	{
+		fprintf(stdout, "Could not open file %s\n", filename);
+		return false;
+	}
+
+	while (fgets(line, MAX_LINE_LEN, pFile) != NULL)
+	{
+		numLines++;
+		switch (line[0])
+	    {
+			case 'c':   // Comments
+			case '\n':  // Line jump
+			case '\0':  // Null character/ end of line
+			default:
+				break;
+				
+            //
+            //  Read size of nodes and edges
+            //
+			case 'p':
+				sscanf(line, "%c %d %d", &c, &declaredNumOfNodes, &declaredNumOfEdges);
+				cout << "Number of nodes is "<< declaredNumOfNodes << " and number of edges is "<< declaredNumOfEdges << endl;
+				init_graph(declaredNumOfNodes, declaredNumOfEdges);
+				break;
+
+			//
+			// Read Nodes
+			//
+			case 'n':
+				sscanf(line, "%c %d %lf %lf ", &c, &nodeId1, &capacity, &capacity2);
+				if (capacity == 0.f && capacity2 == 0.f) 
+				{
+					cout<< "In node "<<nodeId1<<" the excess is "<<capacity<<" and the deficit is "<< capacity2 << endl; 
+					break;
+				}
+				// Add values to f per node(if connected to source or sink)		
+				else f[nodeId1] += capacity2 - capacity;
+				cout<< "In node "<<nodeId1<<" the excess is "<<capacity<<" and the deficit is "<< capacity2 << endl; 
+				break;
+
+			//
+			// Read Edges
+			//
+			case 'a':
+				if (currentNumOfEdges >= declaredNumOfEdges) 
+				{
+					fprintf(stdout, "Number of edges in file does not match (Line %d)\n", numLines);
+					return false;
+				}
+				capacity = 0.f; capacity2 = 0.f;
+				sscanf(line, "%c %d %d %lf %lf", &c, &nodeId1, &nodeId2, &capacity, &capacity2);
+				cout<< "Edge with node1 "<<nodeId1<<" and node2 "<<nodeId2<<" capacity n1n2:  "<< capacity 
+							<<", and capacity n2n1: "<<capacity2<< endl; 
+
+				// Store edges ordering
+				if(nodeId1 < nodeId2){min = nodeId1; max = nodeId2;}
+				else{min = nodeId2; max = nodeId1;} 
+				ord[currentNumOfEdges][0] = min;
+				ord[currentNumOfEdges][1] = max;    	
+
+				// Add values to f and w per edge
+				a = capacity/2.f; b = capacity2/2.f;
+				f[nodeId1] += b - a;
+				f[nodeId2] += a - b;
+				w[nodeId1][nodeId2] = a + b;
+				w[nodeId2][nodeId1] = a + b;
+
+				currentNumOfEdges++;
+				break;
+		}
+	}
+
+	
+	fclose(pFile);
+	pFile = NULL;
+	
+	return true;
+}
+
+// Tell the compiler which T we are going to use
+template class read_bk<float>;
+template class read_bk<double>;
+
