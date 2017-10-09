0409546728695d265fd6252cd6d74e4470c73a7b
summer semester acc#151 <s151@atcremers45.informatik.tu-muenchen.de>
2017-10-09 21:28:03 +0200

    Added README, checked compilation and the results

---
+++ GPU/cmake_build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/main_generated_main.cu.o"
+  "CMakeFiles/main.dir/main_generated_mathOperations.cu.o"
+  "CMakeFiles/main.dir/main_generated_postProcessing.cu.o"
+  "CMakeFiles/main.dir/main_generated_primal_dual.cu.o"
+  "CMakeFiles/main.dir/main_generated_helper.cu.o"
+  "CMakeFiles/main.dir/read_bk.cpp.o"
+  "main.pdb"
+  "main"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
